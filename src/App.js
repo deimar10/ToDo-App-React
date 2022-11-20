5e66303ad3560648b65d11cdcfdaefdae85cfb3c
Deimar <deimarparn14@gmail.com>
2022-11-20 14:01:55 +0200

    Created a skelet for ToDo app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import background from '../src/images/bg-desktop-dark.jpg'
+import sun from '../src/images/icon-sun.svg'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+return (
+<div class="main-section">
+<div class="heading-section" style={{ 
+      backgroundImage: `url(${background})` 
+    }}>
+    <div class="tst">
+  <h1>TODO</h1>
+  <img src={sun} alt=""/>
+  </div>
+</div>
+<div class="body-section">
+  <div class="input-section">
+    <input placeholder="Create a new ToDo"></input>
+  </div>
+  <div class="list">
+    <ul>
+    <li>Jog around</li>
+    <li>Jog around</li>
+    <li>Jog around</li>
+    </ul>
+  </div>
+  <div class="bottom-section">
+    <p id="count">5 Items Left</p>
+    <div id="btns">
+    <button>All</button>
+    <button>Active</button>
+    <button>Completed</button>
     </div>
-  );
+    <button>Clear Completed</button>
+  </div>
+</div>
+</div>
+  )
 }
 
 export default App;
