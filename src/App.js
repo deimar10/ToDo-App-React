db0430daf8ff148a92555dd067806fd05850c43d
Deimar <deimarparn14@gmail.com>
2022-11-21 23:55:25 +0200

    Created a delete function

--- src/App.js
+++ src/App.js
@@ -20,6 +20,13 @@ function App() {
     }
   };
 
+  const deleteTodo = (text) => {
+    const newTodos = todos.filter((todo) => {
+      return todo !== text;
+    });
+    setTodos(newTodos);
+  }
+
   if (themeSwitch === true) {
     imageURL = moon;
   } else {
@@ -71,11 +78,17 @@ function App() {
           <div class="list">
             <ul>
               {todos.map((todo, index) => (
+                <div>
                 <li key={index}
                 style={{
                   borderBottom: themeSwitch === true ? '1px solid  hsl(235, 21%, 11%)' :
                     '1px solid hsl(0, 0%, 98%)'
-                }}> {todo} </li>
+                }}> {todo} </li> 
+
+                <button onClick={() => {
+                  deleteTodo(todo);
+                }}>Delete</button>
+                </div>
               ))}
             </ul>
           </div>
@@ -83,7 +96,7 @@ function App() {
             backgroundColor: themeSwitch === true ? 'hsl(236, 33%, 92%)' :
               'hsl(235, 24%, 19%)'
           }}>
-            <p id="count">0 Items Left</p>
+            <p id="count">{todos.length} Items Left</p>
             <div id="btns">
               <button style={{
                 border: themeSwitch === true ? 'none' :
